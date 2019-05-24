fa3f6f8328e49a301325d84058940609bc659f49
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-05-24 12:03:09 +0100

    Initial commit - added a couple of buttons

---
+++ GitPractice/GitPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Sean Williams on 24/05/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello World")
+    }
+
+
+}
+
